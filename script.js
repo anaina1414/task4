15e50fee0db54cb70dd275858ea16535114c7414
Anaina R <56787091+anaina1414@users.noreply.github.com>
2020-07-30 14:42:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function bulbLight()
+{
+    var image= document.getElementById('bulb');
+       if(image.src.match("bulb2.png"))
+          {
+            image.src="glow2.png";
+          }
+        else
+          {
+            image.src="bulb2.png";
+          }
+}
