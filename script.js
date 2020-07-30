311ecbf8a919456cff4ef2504126a80a0f85510c
Anaina R <56787091+anaina1414@users.noreply.github.com>
2020-07-30 15:03:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function bulbLight()
 {
     var image= document.getElementById('bulb');
-       if(image.src.match("bulb2.png"))
+       if(image.src.match("./bulb2.png"))
           {
-            image.src="glow2.png";
+            image.src="./glow2.png";
           }
         else
           {
-            image.src="bulb2.png";
+            image.src="./bulb2.png";
           }
 }
